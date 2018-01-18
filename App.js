8e35485f1ad66d9b8e08d59b4fc144480db222cd
Adrien Lacquemant <verblodung@gmail.com>
2018-01-18 17:01:38 -0500

    feat: adding search bar in header

--- App.js
+++ App.js
@@ -1,6 +1,14 @@
 import React, { Component } from 'react'
-import { Platform, StyleSheet, Text, View } from 'react-native'
-import { Header } from 'native-base'
+import { Platform, StyleSheet, View } from 'react-native'
+import {
+  Header,
+  Item,
+  Icon,
+  Container,
+  Text,
+  Content,
+  Input
+} from 'native-base'
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -10,32 +18,82 @@ const instructions = Platform.select({
 })
 
 export default class App extends Component<{}> {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      latitude: null,
+      longitude: null,
+      error: null,
+      searchInput: '',
+      searchResult: [],
+      isLoading: false
+    }
+    this.handleSearch = this.handleSearch.bind(this)
+  }
+
+  componentDidMount() {
+    navigator.geolocation.getCurrentPosition(
+      position => {
+        this.setState({
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude,
+          error: null
+        })
+      },
+      error => this.setState({ error: error.message }),
+      { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 }
+    )
+  }
+
+  handleSearch(text) {
+    const placesAutocomplete = text => {
+      fetch(
+        'https://maps.googleapis.com/maps/api/place/autocomplete/json?key=AIzaSyBa2s7Y4_idfCl6UQOhAOJtasI01mQwv0g&input=' +
+          text
+      ).then(response => {
+        const jsonPredictions = JSON.parse(response._bodyInit).predictions
+        this.setState({ searchResult: jsonPredictions })
+      })
+    }
+
+    if (text.length > 2 && !this.state.isLoading) {
+      this.setState({ searchInput: text, isLoading: true })
+      placesAutocomplete(text)
+      setTimeout(() => {
+        this.setState({ isLoading: false })
+        placesAutocomplete(this.state.searchInput)
+      }, 2000)
+    } else {
+      this.setState({ searchInput: text })
+    }
+  }
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to Stops!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
+      <Container>
+        <Header searchBar rounded>
+          <Item>
+            <Icon name="md-map" />
+            <Input
+              placeholder="Enter a stop, or an address"
+              onChangeText={text => this.handleSearch(text)}
+              value={this.state.searchInput}
+            />
+            {this.state.searchInput ? (
+              <Icon
+                name="ios-close-circle"
+                onPress={() => this.setState({ searchInput: '' })}
+              />
+            ) : (
+              <Text />
+            )}
+          </Item>
+        </Header>
+        <Content>
+          <Text>Welcome to Stops!</Text>
+        </Content>
+      </Container>
     )
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF'
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5
-  }
-})
