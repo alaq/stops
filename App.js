3dc0213e5802e759035b6bffea49cbc341a9bd8f
Adrien Lacquemant <verblodung@gmail.com>
2018-01-18 18:28:21 -0500

    feat: adding button to select destination on detail page

--- App.js
+++ App.js
@@ -167,6 +167,12 @@ const DetailsScreen = ({ navigation }) => (
     <H1>{navigation.state.params.place.structured_formatting.main_text}</H1>
     <Text>{navigation.state.params.place.structured_formatting.secondary_text}</Text>
     <Text />
+    <Text />
+    <Button large dark block iconLeft style={{ alignSelf: 'auto' }} onPress={() => navigation.goBack(null)}>
+      <Icon name="bus" />
+      <Text>I am going there</Text>
+    </Button>
+    <Text />
     <Button style={{ alignSelf: 'auto' }} onPress={() => navigation.goBack(null)}>
       <Text>Go back</Text>
     </Button>
