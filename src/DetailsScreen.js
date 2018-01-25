7cdf332ba263f29a9e725bd8426187aa4dc4e992
Adrien Lacquemant <verblodung@gmail.com>
2018-01-25 15:32:48 -0500

    feat: placeholder map

--- src/DetailsScreen.js
+++ src/DetailsScreen.js
@@ -1,33 +1,34 @@
 import React from 'react'
-import { View } from 'react-native'
+import { View, ImageBackground } from 'react-native'
 import { Text, H1, Button, Icon } from 'native-base'
 
 const DetailsScreen = ({ navigation }) => (
-  <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-    <H1>{navigation.state.params.place.name}</H1>
-    {/* <Text>{navigation.state.params.place.structured_formatting.secondary_text}</Text> */}
-    <Text />
-    <Text />
-    <Button
-      large
-      dark
-      block
-      iconLeft
-      style={{ alignSelf: 'auto' }}
-      onPress={() => {
-        // console.log('desti', navigation.state.params.place.name)
-        navigation.state.params.setDestination(navigation.state.params.place)
-        navigation.goBack(null)
-      }}
-    >
-      <Icon name="bus" />
-      <Text>I am going there</Text>
-    </Button>
-    <Text />
-    <Button style={{ alignSelf: 'auto' }} onPress={() => navigation.goBack(null)}>
-      <Text>Go back</Text>
-    </Button>
-  </View>
+  <ImageBackground source={require('../map.jpg')} style={{ width: '100%', height: '100%' }}>
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', zIndex: 2 }}>
+      <H1>{navigation.state.params.place.name}</H1>
+      <Text>Estimated time to destination: 45 minutes</Text>
+      <Text />
+      <Text />
+      <Button
+        large
+        dark
+        block
+        iconLeft
+        style={{ alignSelf: 'auto' }}
+        onPress={() => {
+          navigation.state.params.setDestination(navigation.state.params.place)
+          navigation.goBack(null)
+        }}
+      >
+        <Icon name="bus" />
+        <Text>I am going there</Text>
+      </Button>
+      <Text />
+      <Button style={{ alignSelf: 'auto' }} onPress={() => navigation.goBack(null)}>
+        <Text>Go back</Text>
+      </Button>
+    </View>
+  </ImageBackground>
 )
 
 export default DetailsScreen
