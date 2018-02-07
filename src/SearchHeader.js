3260d243b41667c01453a9e1c77f09cbdcd0f900
Adrien Lacquemant <verblodung@gmail.com>
2018-02-07 16:00:03 -0500

    fix: adding box shadow on search field

--- src/SearchHeader.js
+++ src/SearchHeader.js
@@ -5,7 +5,7 @@ import { Header, Item, Icon, Input, Text } from 'native-base'
 const SearchHeader = props => (
   <Header searchBar rounded>
     <StatusBar barStyle="dark-content" />
-    <Item>
+    <Item style={{ shadowColor: 'black', shadowOpacity: 0.075, shadowRadius: 5, shadowOffset: { width: 0, height: 0 } }}>
       <Icon name="ios-search" />
       <Input placeholder="Enter a stop, or an address" onChangeText={text => props.handleSearch(text)} value={props.searchInput} />
       {props.searchInput ? <Icon name="ios-close-circle" onPress={props.clearSearch} /> : <Text />}
